88396f39b6fad158923919d6db67202576f3a225
ArxAlfa <aaa129@alu.ua.es>
2023-10-28 18:29:32 +0200

    Formato

--- src/train.py
+++ src/train.py
@@ -1,4 +1,3 @@
-
 from model.LightGCN import LightGCN
 from utils.DatasetUtils import DatasetUtils
 
@@ -25,31 +24,27 @@ def main():
 
     edge_index, edge_values = DatasetUtils.load_edge_csv(
         rating_df,
-        src_index_col='userId',
-        dst_index_col='movieId',
-        link_index_col='rating',
-        rating_threshold=1 # need to use threshold=1 so the model can learn based on RMSE
+        src_index_col="userId",
+        dst_index_col="movieId",
+        link_index_col="rating",
+        rating_threshold=1,  # need to use threshold=1 so the model can learn based on RMSE
     )
 
-    edge_index = torch.LongTensor(edge_index) 
+    edge_index = torch.LongTensor(edge_index)
     edge_values = torch.tensor(edge_values)
 
-    num_users = len(rating_df['userId'].unique())
-    num_movies = len(rating_df['movieId'].unique())
+    num_users = len(rating_df["userId"].unique())
+    num_movies = len(rating_df["movieId"].unique())
 
     num_interactions = edge_index.shape[1]
     all_indices = [i for i in range(num_interactions)]
 
     train_indices, test_indices = train_test_split(
-        all_indices, 
-        test_size=0.2, 
-        random_state=1
+        all_indices, test_size=0.2, random_state=1
     )
 
     val_indices, test_indices = train_test_split(
-        test_indices, 
-        test_size=0.5, 
-        random_state=1
+        test_indices, test_size=0.5, random_state=1
     )
 
     train_edge_index = edge_index[:, train_indices]
@@ -61,14 +56,27 @@ def main():
     test_edge_index = edge_index[:, test_indices]
     test_edge_value = edge_values[test_indices]
 
-    train_edge_index, train_edge_values  = DatasetUtils.convert_r_mat_edge_index_to_adj_mat_edge_index(train_edge_index, train_edge_value, num_users, num_movies)
-    val_edge_index, val_edge_values = DatasetUtils.convert_r_mat_edge_index_to_adj_mat_edge_index(val_edge_index, val_edge_value, num_users, num_movies)
-    test_edge_index, test_edge_values = DatasetUtils.convert_r_mat_edge_index_to_adj_mat_edge_index(test_edge_index, test_edge_value, num_users, num_movies)
+    (
+        train_edge_index,
+        train_edge_values,
+    ) = DatasetUtils.convert_r_mat_edge_index_to_adj_mat_edge_index(
+        train_edge_index, train_edge_value, num_users, num_movies
+    )
+    (
+        val_edge_index,
+        val_edge_values,
+    ) = DatasetUtils.convert_r_mat_edge_index_to_adj_mat_edge_index(
+        val_edge_index, val_edge_value, num_users, num_movies
+    )
+    (
+        test_edge_index,
+        test_edge_values,
+    ) = DatasetUtils.convert_r_mat_edge_index_to_adj_mat_edge_index(
+        test_edge_index, test_edge_value, num_users, num_movies
+    )
 
-    layers = 1 
-    model = LightGCN(num_users=num_users, 
-                    num_items=num_movies, 
-                    K=layers)
+    layers = 1
+    model = LightGCN(num_users=num_users, num_items=num_movies, K=layers)
 
     # define contants
     ITERATIONS = 1
@@ -83,9 +91,8 @@ def main():
     LAMBDA = 1e-6
 
     # setup
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device {device}.")
-
 
     model = model.to(device)
     model.train()
@@ -99,14 +106,27 @@ def main():
     train_edge_index = train_edge_index.to(device)
     val_edge_index = val_edge_index.to(device)
 
-
     loss_func = nn.MSELoss()
-    
-    r_mat_train_edge_index, r_mat_train_edge_values = DatasetUtils.convert_adj_mat_edge_index_to_r_mat_edge_index(train_edge_index, train_edge_values, num_users, num_movies)
-    r_mat_val_edge_index, r_mat_val_edge_values = DatasetUtils.convert_adj_mat_edge_index_to_r_mat_edge_index(val_edge_index, val_edge_values, num_users, num_movies)
-    r_mat_test_edge_index, r_mat_test_edge_values = DatasetUtils.convert_adj_mat_edge_index_to_r_mat_edge_index(test_edge_index, test_edge_values, num_users, num_movies)
 
-    
+    (
+        r_mat_train_edge_index,
+        r_mat_train_edge_values,
+    ) = DatasetUtils.convert_adj_mat_edge_index_to_r_mat_edge_index(
+        train_edge_index, train_edge_values, num_users, num_movies
+    )
+    (
+        r_mat_val_edge_index,
+        r_mat_val_edge_values,
+    ) = DatasetUtils.convert_adj_mat_edge_index_to_r_mat_edge_index(
+        val_edge_index, val_edge_values, num_users, num_movies
+    )
+    (
+        r_mat_test_edge_index,
+        r_mat_test_edge_values,
+    ) = DatasetUtils.convert_adj_mat_edge_index_to_r_mat_edge_index(
+        test_edge_index, test_edge_values, num_users, num_movies
+    )
+
     # training loop
     train_losses = []
     val_losses = []
@@ -116,12 +136,12 @@ def main():
         # forward propagation
 
         # the rating here is based on r_mat
-        pred_ratings = model.forward(train_edge_index, train_edge_values, num_users, num_movies)
-        
+        pred_ratings = model.forward(
+            train_edge_index, train_edge_values, num_users, num_movies
+        )
 
-        train_loss = loss_func(pred_ratings, r_mat_train_edge_values.view(-1,1))    
+        train_loss = loss_func(pred_ratings, r_mat_train_edge_values.view(-1, 1))
 
-            
         optimizer.zero_grad()
         train_loss.backward()
         optimizer.step()
@@ -131,51 +151,58 @@ def main():
             model.eval()
 
             with torch.no_grad():
-                val_pred_ratings = model.forward(val_edge_index, val_edge_values, num_users, num_movies)
-                
-                val_loss = loss_func(val_pred_ratings, r_mat_val_edge_values.view(-1,1)).sum()
-                
-                recall_at_k, precision_at_k = LightGCN.get_recall_at_k(r_mat_val_edge_index, 
-                                                            r_mat_val_edge_values, 
-                                                            val_pred_ratings, 
-                                                            k = 20
-                                                            )
-        
-                    
+                val_pred_ratings = model.forward(
+                    val_edge_index, val_edge_values, num_users, num_movies
+                )
+
+                val_loss = loss_func(
+                    val_pred_ratings, r_mat_val_edge_values.view(-1, 1)
+                ).sum()
+
+                recall_at_k, precision_at_k = LightGCN.get_recall_at_k(
+                    r_mat_val_edge_index, r_mat_val_edge_values, val_pred_ratings, k=20
+                )
+
                 val_recall_at_ks.append(round(recall_at_k, 5))
                 train_losses.append(train_loss.item())
                 val_losses.append(val_loss.item())
-            
-                print(f"[Iteration {iter}/{ITERATIONS}], train_loss: {round(train_loss.item(), 5)}, val_loss: {round(val_loss.item(), 5)},  recall_at_k {round(recall_at_k, 5)}, precision_at_k {round(precision_at_k, 5)}")
+
+                print(
+                    f"[Iteration {iter}/{ITERATIONS}], train_loss: {round(train_loss.item(), 5)}, val_loss: {round(val_loss.item(), 5)},  recall_at_k {round(recall_at_k, 5)}, precision_at_k {round(precision_at_k, 5)}"
+                )
 
             model.train()
 
         if iter % ITERS_PER_LR_DECAY == 0 and iter != 0:
             scheduler.step()
-            
+
     iters = [iter * ITERS_PER_EVAL for iter in range(len(train_losses))]
-    plt.plot(iters, train_losses, label='train')
-    plt.plot(iters, val_losses, label='validation')
-    plt.xlabel('iteration')
-    plt.ylabel('loss')
-    plt.title('training and validation loss curves')
+    plt.plot(iters, train_losses, label="train")
+    plt.plot(iters, val_losses, label="validation")
+    plt.xlabel("iteration")
+    plt.ylabel("loss")
+    plt.title("training and validation loss curves")
     plt.legend()
     plt.show()
-    
+
     f2 = plt.figure()
-    plt.plot(iters, val_recall_at_ks, label='recall_at_k')
-    plt.xlabel('iteration')
-    plt.ylabel('recall_at_k')
-    plt.title('recall_at_k curves')
+    plt.plot(iters, val_recall_at_ks, label="recall_at_k")
+    plt.xlabel("iteration")
+    plt.ylabel("recall_at_k")
+    plt.title("recall_at_k curves")
     plt.show()
-    
+
     model.eval()
     with torch.no_grad():
-        pred_ratings = model.forward(test_edge_index, test_edge_values, num_users, num_movies)
-        recall_at_k, precision_at_k = LightGCN.get_recall_at_k(r_mat_test_edge_index, 
-                                                    r_mat_test_edge_values, 
-                                                    pred_ratings, 20)
-        print(f"recall_at_k {round(recall_at_k, 5)}, precision_at_k {round(precision_at_k, 5)}")
+        pred_ratings = model.forward(
+            test_edge_index, test_edge_values, num_users, num_movies
+        )
+        recall_at_k, precision_at_k = LightGCN.get_recall_at_k(
+            r_mat_test_edge_index, r_mat_test_edge_values, pred_ratings, 20
+        )
+        print(
+            f"recall_at_k {round(recall_at_k, 5)}, precision_at_k {round(precision_at_k, 5)}"
+        )
 
 
 if __name__ == "__main__":
--- src/utils/DatasetUtils.py
+++ src/utils/DatasetUtils.py
@@ -1,5 +1,4 @@
-
-#import numpy as np
+# import numpy as np
 import torch
 from torch_geometric.data import download_url, extract_zip
 from torch_sparse import SparseTensor
@@ -7,11 +6,9 @@ from torch_sparse import SparseTensor
 
 class DatasetUtils:
     # load edges between users and movies
-    def load_edge_csv(df, 
-                    src_index_col, 
-                    dst_index_col,  
-                    link_index_col, 
-                    rating_threshold=3.5):
+    def load_edge_csv(
+        df, src_index_col, dst_index_col, link_index_col, rating_threshold=3.5
+    ):
         """Loads csv containing edges between users and items
 
         Args:
@@ -26,17 +23,20 @@ class DatasetUtils:
         Returns:
             torch.Tensor: 2 by N matrix containing the node ids of N user-item edges
         """
-        
-        edge_index = None
-        src = [user_id for user_id in  df['userId']]
-        
-        num_users = len(df['userId'].unique())
 
-        dst = [(movie_id) for movie_id in df['movieId']]
-        
+        edge_index = None
+        src = [user_id for user_id in df["userId"]]
+
+        num_users = len(df["userId"].unique())
+
+        dst = [(movie_id) for movie_id in df["movieId"]]
+
         link_vals = df[link_index_col].values
 
-        edge_attr = torch.from_numpy(df[link_index_col].values).view(-1, 1).to(torch.long) >= rating_threshold
+        edge_attr = (
+            torch.from_numpy(df[link_index_col].values).view(-1, 1).to(torch.long)
+            >= rating_threshold
+        )
 
         edge_values = []
 
@@ -47,55 +47,59 @@ class DatasetUtils:
                 edge_index[1].append(dst[i])
                 edge_values.append(link_vals[i])
 
-        # edge_values is the label we will use for compute training loss                
+        # edge_values is the label we will use for compute training loss
         return edge_index, edge_values
 
-
-    def convert_r_mat_edge_index_to_adj_mat_edge_index(input_edge_index, input_edge_values, num_users, num_movies):
+    def convert_r_mat_edge_index_to_adj_mat_edge_index(
+        input_edge_index, input_edge_values, num_users, num_movies
+    ):
         R = torch.zeros((num_users, num_movies))
         for i in range(len(input_edge_index[0])):
             row_idx = input_edge_index[0][i]
             col_idx = input_edge_index[1][i]
-            R[row_idx][col_idx] = input_edge_values[i] # assign actual edge value to Interaction Matrix
+            R[row_idx][col_idx] = input_edge_values[
+                i
+            ]  # assign actual edge value to Interaction Matrix
 
         R_transpose = torch.transpose(R, 0, 1)
-        
+
         # create adj_matrix
-        adj_mat = torch.zeros((num_users + num_movies , num_users + num_movies))
-        adj_mat[: num_users, num_users :] = R.clone()
-        adj_mat[num_users :, : num_users] = R_transpose.clone()
-        
+        adj_mat = torch.zeros((num_users + num_movies, num_users + num_movies))
+        adj_mat[:num_users, num_users:] = R.clone()
+        adj_mat[num_users:, :num_users] = R_transpose.clone()
+
         adj_mat_coo = adj_mat.to_sparse_coo()
         adj_mat_coo_indices = adj_mat_coo.indices()
         adj_mat_coo_values = adj_mat_coo.values()
         return adj_mat_coo_indices, adj_mat_coo_values
 
+    def convert_adj_mat_edge_index_to_r_mat_edge_index(
+        input_edge_index, input_edge_values, num_users, num_movies
+    ):
+        sparse_input_edge_index = SparseTensor(
+            row=input_edge_index[0],
+            col=input_edge_index[1],
+            value=input_edge_values,
+            sparse_sizes=((num_users + num_movies), num_users + num_movies),
+        )
 
-    def convert_adj_mat_edge_index_to_r_mat_edge_index(input_edge_index, input_edge_values, num_users, num_movies):
-        
-        sparse_input_edge_index = SparseTensor(row=input_edge_index[0], 
-                                            col=input_edge_index[1], 
-                                            value = input_edge_values,
-                                            sparse_sizes=((num_users + num_movies), num_users + num_movies))
-        
         adj_mat = sparse_input_edge_index.to_dense()
-        interact_mat = adj_mat[: num_users, num_users :]
-        
+        interact_mat = adj_mat[:num_users, num_users:]
+
         r_mat_edge_index = interact_mat.to_sparse_coo().indices()
         r_mat_edge_values = interact_mat.to_sparse_coo().values()
-        
+
         return r_mat_edge_index, r_mat_edge_values
-    
-    
+
     def load_dataset():
         # download the dataset
         # https://grouplens.org/datasets/movielens/
         # "Small: 100,000 ratings and 3,600 tag applications applied to 9,000 movies by 600 users. Last updated 9/2018"
-        url = 'https://files.grouplens.org/datasets/movielens/ml-latest-small.zip'
-        extract_zip(download_url(url, '.'), '.')
-        
-        movie_path = './ml-latest-small/movies.csv'
-        rating_path = './ml-latest-small/ratings.csv'
-        user_path = './ml-latest-small/users.csv'
-        
+        url = "https://files.grouplens.org/datasets/movielens/ml-latest-small.zip"
+        extract_zip(download_url(url, "."), ".")
+
+        movie_path = "./ml-latest-small/movies.csv"
+        rating_path = "./ml-latest-small/ratings.csv"
+        user_path = "./ml-latest-small/users.csv"
+
         return movie_path, rating_path, user_path
